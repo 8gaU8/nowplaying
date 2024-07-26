4a192748d47a4e967af48abdfa50ba96d592ef13
8gaU8 <haga.yuya2.2016@gmail.com>
2024-07-27 01:58:24 +0900

    Redirect URLの変更

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const redirect_uri = 'http://localhost:8000'; // GitHub PagesのURLを入力
+const redirect_uri = 'https://8gaU8.github.io/nowplaying'; // GitHub PagesのURLを入力
 
 const authEndpoint = 'https://accounts.spotify.com/authorize';
 const scopes = [
@@ -8,7 +8,8 @@ const scopes = [
 document.getElementById('save-client-id').addEventListener('click', () => {
     const clientId = document.getElementById('client-id').value;
     const discogsToken = document.getElementById('discogs-token').value;
-    if (clientId && discogsToken) {
+    if (clientId && discogsToken)
+    {
         localStorage.setItem('spotify_client_id', clientId);
         localStorage.setItem('discogs_token', discogsToken);
         window.location.reload();
@@ -17,7 +18,8 @@ document.getElementById('save-client-id').addEventListener('click', () => {
 
 document.getElementById('login-button').addEventListener('click', () => {
     const clientId = localStorage.getItem('spotify_client_id');
-    if (clientId) {
+    if (clientId)
+    {
         window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirect_uri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
     }
 });
@@ -32,7 +34,8 @@ function getAccessToken() {
         .substring(1)
         .split('&')
         .reduce((initial, item) => {
-            if (item) {
+            if (item)
+            {
                 let parts = item.split('=');
                 initial[parts[0]] = decodeURIComponent(parts[1]);
             }
@@ -42,9 +45,11 @@ function getAccessToken() {
 
     let _token = hash.access_token;
 
-    if (_token) {
+    if (_token)
+    {
         localStorage.setItem('spotify_access_token', _token);
-    } else {
+    } else
+    {
         _token = localStorage.getItem('spotify_access_token');
     }
 
@@ -60,20 +65,23 @@ async function fetchPersonnel(track, artist) {
     const discogsToken = localStorage.getItem('discogs_token');
     const url = `https://api.discogs.com/database/search?q=${encodeURIComponent(query)}&token=${discogsToken}`;
 
-    try {
+    try
+    {
         const response = await fetch(url);
         const data = await response.json();
-        
-        if (data.results.length > 0) {
+
+        if (data.results.length > 0)
+        {
             const masterUrl = data.results[0].master_url;
-            if (masterUrl) {
+            if (masterUrl)
+            {
                 const masterResponse = await fetch(masterUrl);
                 const masterData = await masterResponse.json();
-                
+
                 const personnelList = masterData.tracklist
                     .filter(trackItem => cleanTrackName(trackItem.title.toLowerCase()) === track.toLowerCase())
                     .flatMap(trackItem => trackItem.extraartists || []);
-                
+
                 const personnelListElement = document.getElementById('personnel-list');
                 personnelListElement.innerHTML = '';
                 personnelList.forEach(person => {
@@ -83,7 +91,8 @@ async function fetchPersonnel(track, artist) {
                 });
             }
         }
-    } catch (error) {
+    } catch (error)
+    {
         console.error('Error fetching Discogs data:', error);
     }
 }
@@ -106,27 +115,32 @@ window.addEventListener('load', async () => {
     const clientId = localStorage.getItem('spotify_client_id');
     const discogsToken = localStorage.getItem('discogs_token');
 
-    if (!clientId || !discogsToken) {
+    if (!clientId || !discogsToken)
+    {
         document.getElementById('client-id-form').style.display = 'block';
         document.getElementById('login').style.display = 'none';
         document.getElementById('player').style.display = 'none';
-    } else {
+    } else
+    {
         const token = getAccessToken();
 
-        if (token) {
+        if (token)
+        {
             document.getElementById('client-id-form').style.display = 'none';
             document.getElementById('login').style.display = 'none';
             document.getElementById('player').style.display = 'block';
 
-            try {
+            try
+            {
                 const response = await fetch('https://api.spotify.com/v1/me/player', {
                     headers: {
                         'Authorization': `Bearer ${token}`
                     }
                 });
                 const data = await response.json();
-                
-                if (data && data.is_playing) {
+
+                if (data && data.is_playing)
+                {
                     const track = data.item;
                     const trackDetails = await getEnglishTrackDetails(track.id, token);
 
@@ -136,13 +150,16 @@ window.addEventListener('load', async () => {
                     document.getElementById('album-art').src = track.album.images[0].url;
 
                     fetchPersonnel(trackDetails.name, trackDetails.artist);
-                } else {
+                } else
+                {
                     document.getElementById('track-name').innerText = 'No track currently playing';
                 }
-            } catch (error) {
+            } catch (error)
+            {
                 console.error('Error fetching Spotify data:', error);
             }
-        } else {
+        } else
+        {
             document.getElementById('client-id-form').style.display = 'none';
             document.getElementById('login').style.display = 'block';
             document.getElementById('player').style.display = 'none';
